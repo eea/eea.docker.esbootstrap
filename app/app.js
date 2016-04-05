1f0a307a53c0253069a4c8d830d2054f6dcad03f
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2016-04-05 16:10:44 +0300

    Refs #70021; Removed some hardcoded parameters: endpoint, filters query, analyzers

--- app/app.js
+++ app/app.js
@@ -9,6 +9,12 @@ var searchServer = require('eea-searchserver')
 var managementCommands = searchServer.builtinCommands;
 var builtinRoutes = searchServer.builtinRoutes;
 
+var defaultIndexingFilterQuery = 'config/filtersQuery.sparql';
+var defaultExtraAnalyzers = 'config/analyzers.json';
+
+var nconf = require('nconf');
+nconf.file({file:'/code/config/settings.json'});
+var endpoint = nconf.get("endpoint");
 options = {
   app_dir: __dirname,
   views: __dirname + '/views',
@@ -17,14 +23,21 @@ options = {
     routes: builtinRoutes,
     detailsIdName: 'id'
   },
+
   indexing:{
     managementCommands: managementCommands,
     indexingFilterQuery: null,
     indexingQuery: 'config/query.sparql',
     extraAnalyzers: '',
     dataMapping: 'config/dataMapping.json',
-    endpoint: 'http://semantic.eea.europa.eu/sparql',
+    endpoint: endpoint
   }
+}
+if (fs.existsSync(__dirname +'/' + defaultIndexingFilterQuery)){
+  options.indexing.indexingFilterQuery = defaultIndexingFilterQuery;
+}
+if (fs.existsSync(__dirname +'/' + defaultExtraAnalyzers)){
+  options.indexing.extraAnalyzers = defaultExtraAnalyzers;
 }
 searchServer.Helpers.SimpleStart(options)
 
