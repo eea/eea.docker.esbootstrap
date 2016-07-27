22f0ca24f2be9939a891f88745f5255e81355745
Dragos Chirila <dragos.chirila@eaudeweb.ro>
2016-07-27 11:50:52 +0300

    Added "sort by" widget and display view selection.
    refs #73715

--- app/public/javascripts/esbootstrap.facetview.js
+++ app/public/javascripts/esbootstrap.facetview.js
@@ -61,6 +61,20 @@ jQuery(document).ready(function($) {
             enable_rangeselect: true,
             enable_geoselect: true,
             default_sort: default_sort,
+            search_sortby: [
+              {
+                'field': 'title',
+                'display_asc': 'Title a-z',
+                'display_desc': 'Title z-a'
+              },
+              {
+                'field': 'created',
+                'display_asc': 'Oldest',
+                'display_desc': 'Newest'
+              }
+            ],
+            sort: [{'created': {'order': 'desc'}}
+            ],
             post_init_callback: function() {
               add_EEA_settings();
               replaceNumbers();
