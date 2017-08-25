0c6e3cc94150e1efc5a20b666af80792901ed8cc
David Ichim <ichim.david@gmail.com>
2017-08-25 11:45:59 +0200

    Refs #87455 added custom post init and search events and allow extending of options

--- app/public/javascripts/esbootstrap.facetview.js
+++ app/public/javascripts/esbootstrap.facetview.js
@@ -1,30 +1,34 @@
 jQuery(document).ready(function($) {
-    var default_sort = [];
-    eea_facetview('.facet-view-simple', 
-        {
-            search_url: './api',
-            search_index: 'elasticsearch',
-            datatype: 'json',
-            initial_search: false,
-            enable_rangeselect: true,
-            enable_geoselect: true,
-            default_sort: default_sort,
-            search_sortby: settings_search_sortby,
-            sort: settings_sort,
-            post_init_callback: function() {
-              add_EEA_settings();
-              replaceNumbers();
-            },
-            post_search_callback: function() {
-              add_EEA_settings();
-              viewReady();
-              replaceNumbers();
-            },
-            paging: {
-                from: 0,
-                size: 10
-            },
-            display_type_options: settings_display_options,
-            display_type: settings_default_display
-        });
+    var opts = {
+        search_url: './api',
+        search_index: 'elasticsearch',
+        datatype: 'json',
+        initial_search: false,
+        enable_rangeselect: true,
+        enable_geoselect: true,
+        default_sort: [],
+        search_sortby: settings_search_sortby,
+        sort: settings_sort,
+        post_init_callback: function() {
+            add_EEA_settings();
+            replaceNumbers();
+            $(window).trigger('eea_post_init_callback');
+        },
+        post_search_callback: function() {
+            add_EEA_settings();
+            viewReady();
+            replaceNumbers();
+            $(window).trigger('eea_post_search_callback');
+        },
+        paging: {
+            from: 0,
+            size: 10
+        },
+        display_type_options: settings_display_options,
+        display_type: settings_default_display
+    };
+    if (window.esbootstrap_options) {
+       $.extend(opts, esbootstrap_options);
+    }
+    eea_facetview('.facet-view-simple', opts);
 });
